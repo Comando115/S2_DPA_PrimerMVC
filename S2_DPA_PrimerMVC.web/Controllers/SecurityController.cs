fbd983d7db7d369720f9fc14e1cf4a303b4c2279
Comando115 <90572759+Comando115@users.noreply.github.com>
2021-10-17 08:32:06 -0500

    Agregar archivos de proyecto.

---
+++ S2_DPA_PrimerMVC.web/Controllers/SecurityController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S2_DPA_PrimerMVC.web.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login(string email,string password) 
+        {
+
+            if (email == "mezajose@hotmail.com" && password == "1254") 
+            {
+                return RedirectToAction("Index", "Home", new { area = "Marketing" });
+            
+             }
+
+            return RedirectToAction("Index"); 
+
+        }
+    }
+}
